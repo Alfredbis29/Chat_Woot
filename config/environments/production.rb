7cb637d1d4c1c8093d9fe63efeaba892ff57a01e
Alfred <kalumunabisimwa5@gmail.com>
2026-01-28 22:58:32 +0200

    Add help center style home page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -11,7 +10,6 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
