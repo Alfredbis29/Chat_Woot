f9e10a34fbd1448ccddc3f703ffc029a89f9af66
Alfred <kalumunabisimwa5@gmail.com>
2026-01-31 22:54:03 +0200

    Add navigation functionality with article pages, search, and contact support

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'articles/:slug', to: 'articles#show', as: 'article'
+  get 'search', to: 'home#search', as: 'search'
+  get 'contact', to: 'home#contact', as: 'contact'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
