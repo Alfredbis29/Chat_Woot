1810a9c9aa0dd3fbadc15a63f7262934fe949ea3
Alfred <kalumunabisimwa5@gmail.com>
2026-02-04 16:12:17 +0200

    Contribution 7: Add categories browsing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'categories/index'
   get 'home/index'
   get 'articles/:slug', to: 'articles#show', as: 'article'
   get 'search', to: 'home#search', as: 'search'
