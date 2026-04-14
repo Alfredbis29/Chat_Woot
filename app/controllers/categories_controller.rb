05dc32bac715f3ac885c25e39f3e711279c04123
Alfred <kalumunabisimwa5@gmail.com>
2026-04-14 03:41:15 +0200

    Add create action to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,4 +6,14 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to categories_path, notice: 'Category was created successfully.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
