d6b4235d9bcc0f1405164834e620a7288677dd0f
Alfred <kalumunabisimwa5@gmail.com>
2026-04-17 19:15:08 +0200

    Add integration tests for category new and create actions

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,7 +2,27 @@ require "test_helper"
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get categories_index_url
+    get categories_url
     assert_response :success
+  end
+
+  test "should get new" do
+    get new_category_url
+    assert_response :success
+  end
+
+  test "should create category" do
+    assert_difference("Category.count", 1) do
+      post categories_url, params: {
+        category: {
+          name: "Customer Support",
+          description: "Articles for customer support workflows."
+        }
+      }
+    end
+
+    assert_redirected_to categories_url
+    follow_redirect!
+    assert_select "div", /Customer Support/
   end
 end
